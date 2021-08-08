9a9ae2555d5725706a71666ba644302e1851cf02
anurag-singh2001 <anuragrazarwal@gmail.com>
2021-08-08 13:10:52 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,42 @@
+const socket = io('http://localhost:8000')
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container')
+var audio = new Audio('ding-sound.mp3');
+
+
+const append = (message,postion)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(postion);
+    messageContainer.append(messageElement);
+
+    if(postion=='left'){
+        audio.play();
+    }
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`,'right')
+    socket.emit('send',message)
+    messageInput.value = ''
+})
+
+//const Name = prompt("Enter your name to join ");
+socket.emit('new-user-joined',Name)
+
+socket.on('user-joined',Name => {
+    append(`${Name} joined the chat`,'right')
+})
+
+socket.on('receive',data => {
+    append(`${data.Name}: ${data.message} `,'left')
+})
+
+socket.on('left',Name => {
+    append(`${Name}: left the chat `,'left')
+})
